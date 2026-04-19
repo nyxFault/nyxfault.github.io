58823ccf11b599ab1481a1da0f032a09882f44b4
nyxfault <nyxfault@gmail.com>
2026-04-19 08:19:48 -0400

    Syzkaller

--- _plugins/filter_sitemap.rb
+++ _plugins/filter_sitemap.rb
@@ -3,6 +3,13 @@ Jekyll::Hooks.register :site, :post_write do |site|
   if File.exist?(sitemap)
     content = File.read(sitemap)
 
+    # jekyll-sitemap adds xsi attributes on <urlset>; Google Search Console sometimes
+    # reports "Couldn't fetch" for *.github.io until they are removed (jekyll-sitemap#320).
+    content = content.gsub(
+      / xmlns:xsi="[^"]*" xsi:schemaLocation="[^"]*"/,
+      ""
+    )
+
     # remove <url> blocks for /tags/ and /categories/
     filtered = content.gsub(
       %r{<url>\s*<loc>https?://[^<]*/(tags|categories)/.*?</url>}m,
